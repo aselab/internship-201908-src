2ad200067f41e96219b7be4067dd2e7e39fd8941
SoutaMori-ase <gh1010801310+ase@gmail.com>
2019-08-24 07:08:39 +0900

    モブプロの成果追加

--- src/App.js
+++ src/App.js
@@ -1,50 +1,94 @@
 import * as React from 'react';
-import { KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
+import { View,KeyboardAvoidingView, ScrollView, StyleSheet, Text } from 'react-native';
 import StickyNote from './StickyNote';
 import uuidv4 from 'uuid/v4'
 import { Button } from 'react-native-elements';
-
+import PickerSelect from 'react-native-picker-select';
+import moment from 'moment';
 
 export default class App extends React.Component {
 
   state = {
     StickyNoteList: [],
+    Style: styles.whiteContainer,
+    Tags:  [
+                { label: '学校', value: 1 },
+                { label: 'バイト', value: 2 },
+                { label: '重要', value: 3  },
+                { label: 'デート', value: 4  }
+            ],
+    Test: "ダミー"
   };
+
+  setDateTime = (uuid, dateTime) => {
+    var newList = this.state.StickyNoteList;
+    newList.find(note => note.component.key === uuid).limitDateTime = dateTime;
+    this.setState({ StickyNoteList: newList });
+  }
 
   deleteStickyNote = uuid => {
     const stickyNoteList = this.state.StickyNoteList;
-    const newList = stickyNoteList.filter(note => note.key != uuid);
+    const newList = stickyNoteList.filter(note => note.component.key != uuid);
     this.setState({ StickyNoteList: newList });
+    
   }
 
   addStickyNote = () => {
     const stickyNoteList = this.state.StickyNoteList;
     const uuid = uuidv4();
+    
+    stickyNoteList.sort((val1,val2)=>val1.limitDateTime-val2.limitDateTime)
+    stickyNoteList.push({
+      component: <StickyNote key={uuid} uuid={uuid} deleteStickyNote={this.deleteStickyNote} Tags={this.state.Tags} setDateTime={this.setDateTime}/>,
+      limitDateTime: moment(),
+    });
+    this.setState({ StickyNoteList: stickyNoteList});
+  }
+  /*
+  addTags = () => {
+    const Tags = this.state.Tags;
+    const uuid = uuidv4();
     stickyNoteList.push(
-      <StickyNote key={uuid} uuid={uuid} deleteStickyNote={this.deleteStickyNote} />
+     {label: 'de-to',value:uuid}
     );
     this.setState({ StickyNoteList: stickyNoteList });
-  };
+  }*/
+  
 
   render() {
     return (
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          {this.state.StickyNoteList}
-        </ScrollView>
+      <View>
+        <KeyboardAvoidingView style={this.state.Style} behavior="padding">
+          <PickerSelect
+              onValueChange={ (value) => this.setState({ Style:value})}
+              items={[
+                  { label: 'しろ', value: styles.whiteContainer },
+                  { label: 'くろ', value: styles.blackContainer }
+              ]}
+          />
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            {this.state.StickyNoteList.map(note => note.component)}
+          </ScrollView>
+        
+        </KeyboardAvoidingView>
         <Button
           title="＋"
           onPress={this.addStickyNote}
           buttonStyle={styles.addButton}
         />
-      </KeyboardAvoidingView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  whiteContainer: {
     height: '100%',
+    backgroundColor: '#ffffff'
+  },
+  blackContainer: {
+    height: '100%',
+    backgroundColor: '#000000'
   },
   scrollContainer: {
     marginTop: 10,
@@ -56,6 +100,6 @@ const styles = StyleSheet.create({
     left: 20,
     bottom: 20,
     position: 'absolute',
-    borderRadius: 50,
+    //borderRadius: 50,
   },
 });
--- src/StickyNote.js
+++ src/StickyNote.js
@@ -3,25 +3,34 @@ import { Text, TextInput, View, StyleSheet } from 'react-native';
 import moment from 'moment';
 import { Card, Button } from 'react-native-elements';
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import PickerSelect from 'react-native-picker-select';
+
 
 export default class StickyNote extends Component {
   state = {
     isDateTimePickerVisible: false,
-    limitDateTime: moment().format('YYYY年MM月DD日 HH時mm分')
+    limitDateTime: moment().format('YYYY年MM月DD日 HH時mm分'),
+    CardStyle:styles.card,
+    size:1,
+    Title:"",
+    Content:"",
+    Tag:0
   };
 
   showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
   hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
 
-  setDateTime = dateTime =>
+  setDateTime = dateTime => {
+    this.props.setDateTime(this.props.uuid, dateTime);
     this.setState({
       limitDateTime: moment(dateTime).format('YYYY年MM月DD日 HH時mm分'),
     });
+  }
 
   render() {
     return (
       <Card
-        containerStyle={styles.card}
+        containerStyle={this.state.CardStyle}
         title={
           <View>
             <TextInput
@@ -32,9 +41,14 @@ export default class StickyNote extends Component {
           </View>
         }>
         <Button
-          title="×"
+          title="✖"
           buttonStyle={styles.deleteButton}
           onPress={() => this.props.deleteStickyNote(this.props.uuid)}
+        />
+        <Button
+          title="△"
+          buttonStyle={styles.resizeButton}
+          //onPress={() => this.resize()}
         />
         <TextInput
           placeholder="内容を入力"
@@ -58,15 +72,53 @@ export default class StickyNote extends Component {
           }}
           onCancel={this.hideDateTimePicker}
         />
+        <PickerSelect
+            onValueChange={ (value) => {
+              this.setState({Tag : value});
+              switch (value){
+                case 1:
+                  this.setState({CardStyle:styles.schoolcard})
+                  break;
+                case 2:
+                  this.setState({CardStyle:styles.bytecard})
+                  break;
+                case 3:
+                  this.setState({CardStyle:styles.importantcard})
+                  break;
+                case 4:
+                  this.setState({CardStyle:styles.lovecard})
+                  break;
+                default:
+                  this.setState({CardStyle:styles.card})
+              }
+              
+              }
+            }
+            
+            items={this.props.Tags}
+        />
       </Card>
     );
   }
 }
-
+const resize = () =>{
+  this.setState({size:(this.state.size+1)%2})
+}
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#f5f99a',
   },
+  schoolcard: {
+    backgroundColor: '#00bfff',
+  },
+  bytecard: {
+    backgroundColor: '#778899',
+  },
+  importantcard: {
+    backgroundColor: '#ff6347',
+  },
+  lovecard: {
+    backgroundColor: '#ff99ff'},
   deleteButton: {
     backgroundColor: '#EFD032',
     position: 'absolute',
@@ -76,8 +128,17 @@ const styles = StyleSheet.create({
     width: 30,
     borderRadius: 30
   },
+   resizeButton: {
+    backgroundColor: '#EFD032',
+    position: 'absolute',
+    bottom: 0,
+    right: 40,
+    height: 30,
+    width: 30,
+    borderRadius: 30
+  },
   datetimeContainer: {
     marginTop: 10,
     flexDirection: 'row',
   },
-}); 
+});
